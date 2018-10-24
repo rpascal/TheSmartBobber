8a90190f8e332d35e7f2be1f3c7b60292532b91d
Ryan Pascal Desktop <ryanpascal99@yahoo.com>
2018-10-23 22:44:30 -0400

    Adding android capacitor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { CameraResultType, CameraSource, Plugins } from '@capacitor/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-export class HomePage {}
+export class HomePage {
+  image: any;
+
+  async takePhoto() {
+    const capturedImage = await Plugins.Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      source: CameraSource.Camera,
+      resultType: CameraResultType.Uri
+    });
+    this.image = capturedImage;
+  }
+}
