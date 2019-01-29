21f82a4bfe42b5035f5bc1311871363ccb461bbc
zach7953 <39473681+zach7953@users.noreply.github.com>
2019-01-29 16:55:03 -0500

    Updated ADC Code

--- embedded/PIC16LF876A_ADC/main.c
+++ embedded/PIC16LF876A_ADC/main.c
@@ -2,21 +2,31 @@
  * File:   main.c
  * Author: zpyle
  *
- * Created on January 27, 2019, 7:50 PM
+ * Created on January 26, 2019, 12:50 PM
  */
 
-
+//Defining headers that are to be used as well as the config file
 #include <xc.h>
 #include "config.h"
+//Defining Frequency of the oscillator that is being used (4Mhz)
+//Used for the __delay_ms functions
 #define _XTAL_FREQ 4000000
 
+//Initializing the ADC.  See data sheets pages 127-134 (hard copy)
+//See data sheets pages 129-136 (digital copy)
 void ADC_Init()
 {
+   //Turning on the ADCON0 (Binary = 0100 0001)
+   //First Bit turns on ADC, Bit 7/6 makes the Clock Conversion = Fosc/16
    ADCON0 = 0x41;
+   //Turning on the ADCON1 (Binary = 1100 0000)
+   //7th Bit is A/D Result Format Select bit (Right Justified)
+   //6th Bit is dependent on ADCON0 Clock Conversion
    ADCON1 = 0xC0;
 }
 
-unsigned int ADC_Read(unsigned char channel)
+//Declaring the Read_ADC Function in order to Read the ADC
+unsigned int Read_ADC(unsigned char channel)
 {
    if(channel > 7)
      return 0; 
@@ -29,29 +39,30 @@ unsigned int ADC_Read(unsigned char channel)
    return ((ADRESH<<8)+ADRESL);
 }
 
+//Writing the man function
 void main()
 {
-   //Declaring Variables
-    unsigned int a;
-   unsigned int threshold = 61;
-   //Output pins for LEDs
-   TRISB = 0x00;
-   TRISC = 0x00;
-   //Intialize ADC
+   //Declaring Variables to see if we dip below a certain threshold
+   //Using unsigned because they will only hold positive numbers
+   //Declaring Counter for for-loop as well as blank array
+   unsigned int SampleCount;
+   unsigned int ADCSamples[20];
+   
+   //Declaring the B output pins for the purpose of testing the LEDs
+   //TRISB = 0x00;
+   
+   //Calling the function to Initialize ADC
    ADC_Init();
 
-   //Main Loop
+   //Writing the main loop
    while(1)
    {
-     a = ADC_Read(0);
-     if (a<threshold)
+     //Writing a for loop to store the ADC samples into an array to compare them
+     for(SampleCount = 0; SampleCount < 20; SampleCount++)
      {
-         PORTB = 0xff;
+         ADCSamples[SampleCount] = Read_ADC(0);
      }
-     else
-     {
-         PORTB = 0x00;
-     }
-     __delay_ms(100);
-   }
-}
+     
+     
+   }    //End of while(1)
+}   //End of Main
