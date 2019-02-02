eff76545aeb66bd9662ee69d323c8b1f992c7226
zach7953 <39473681+zach7953@users.noreply.github.com>
2019-02-02 12:21:29 -0500

    Updated Main ADC Code for Changes
    Included PWM, and Logic that Ryan helped med with

--- embedded/PIC16LF876A_ADC/main.c
+++ embedded/PIC16LF876A_ADC/main.c
@@ -8,6 +8,8 @@
 //Defining headers that are to be used as well as the config file
 #include <xc.h>
 #include "config.h"
+#include "defines.h"
+
 //Defining Frequency of the oscillator that is being used (4Mhz)
 //Used for the __delay_ms functions
 #define _XTAL_FREQ 4000000
@@ -39,17 +41,55 @@ unsigned int Read_ADC(unsigned char channel)
    return ((ADRESH<<8)+ADRESL);
 }
 
+//--[ Duty Cycle % Input Function ]--
+void PWM1_Set_Duty(unsigned int DC)
+{
+  // Check The DC Value To Make Sure it's Within 10-Bit Range
+  if(DC<1024)
+  {
+    CCP1Y = DC & 1;
+    CCP1X = DC & 2;
+    CCPR1L = DC >> 2;
+  }
+}
+
+//Function for PWM Initialize
+void PWM_Initialize(void)
+{
+   //--[ Configure The CCP Module For PWM Mode ]--
+  //CPP in PWM Mode
+  CCP1M3 = 1;
+  CCP1M2 = 1;
+  // The CCP1 Output Pin (PWM) [RC2]
+  TRISC2 = 0; 
+  // Set The PWM Frequency of 8kHz
+  PR2 = 124; 
+  // Set The PS For Timer2 (1:1 Ratio)
+  T2CKPS0 = 0;
+  T2CKPS1 = 0;
+  // Start CCP1 PWM
+  TMR2ON = 1; //TIMER2 Begins
+  //--[ End of Configuration ]--
+  
+  //--[ Set Duty Cycle to 50% ]--
+  PWM1_Set_Duty(250);//Set duty cycle to 50% 
+}
+
+//Declaring global variable
+unsigned int previous_value = 0;
+
 //Writing the man function
 void main()
 {
    //Declaring Variables to see if we dip below a certain threshold
    //Using unsigned because they will only hold positive numbers
-   //Declaring Counter for for-loop as well as blank array
-   unsigned int SampleCount;
-   unsigned int ADCSamples[20];
+   unsigned int new_value;
    
    //Declaring the B output pins for the purpose of testing the LEDs
-   //TRISB = 0x00;
+   TRISB = 0x00;
+   
+   //Calling the function to Initialize PWM
+   PWM_Initialize();
    
    //Calling the function to Initialize ADC
    ADC_Init();
@@ -57,12 +97,21 @@ void main()
    //Writing the main loop
    while(1)
    {
-     //Writing a for loop to store the ADC samples into an array to compare them
-     for(SampleCount = 0; SampleCount < 20; SampleCount++)
-     {
-         ADCSamples[SampleCount] = Read_ADC(0);
-     }
-     
+    new_value =  Read_ADC(0);
+    unsigned int compare = (new_value - previous_value);
+    previous_value = new_value;
+    
+    //Deciding if the there is large enough value change
+    //To see if the Plunger has been moved
+    if(compare>9)
+    {
+        PORTB=1;
+    }
+    else
+    {
+        PORTB = 0;
+        
+    }
      
    }    //End of while(1)
 }   //End of Main
