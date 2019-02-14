3e70ba147de8b43f85dde83f007fe2c6dd578b5c
NSpoutz <30293581+NSpoutz@users.noreply.github.com>
2019-02-13 20:48:31 -0500

    Update

--- embedded/PIC16LF876A_Main.X/ADC.h
+++ embedded/PIC16LF876A_Main.X/ADC.h
@@ -4,11 +4,14 @@
 void ADC_Initialize(void);
 unsigned int ADC_Read(void);
 <<<<<<< HEAD
+<<<<<<< HEAD
 void monitorSolenoidSignal(void);
 void sendADCToPhone(void);
 void solenoidActiveMonitor(void);
 void turnOnSolenoid(void);
 =======
+=======
+>>>>>>> parent of 9162794... Nick_Update
 void monitorBiteAverage(void);
 void sendBiteDataToPhone(void);
 >>>>>>> parent of 9162794... Nick_Update
