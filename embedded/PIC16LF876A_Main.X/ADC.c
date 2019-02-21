67e34d018ecb50b6d02690ea92e04e232409f07f
NSpoutz <30293581+NSpoutz@users.noreply.github.com>
2019-02-21 14:01:36 -0500

    Make adc compile

--- embedded/PIC16LF876A_Main.X/ADC.c
+++ embedded/PIC16LF876A_Main.X/ADC.c
@@ -4,39 +4,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
-int MAX_SOLENOID_ON = 2;
+float MAX_SOLENOID_ON = 1;
 int count = 0;
 int iterationsPerAverage = 10;
 int mean = 0;
 int oldMean = 0;
 int threshold = 10;
-clock_t startSolenoidOnClock;
+time_t startSolenoidOnClock;
 bool isSolenoidOn = false;
 bool isSolenoidOnMessageTrigger = false;
 bool isSolenoidOffMessageTrigger = false;
 
-void ADC_Initialize(void)
-{
+void ADC_Initialize(void) {
     ADCON0 = 0x41; //ADC ON and Fosc/16 is selected
     ADCON1 = 0xC0; // Internal reference voltage is selected
 }
 
-unsigned int ADC_Read(void)
-{
+unsigned int ADC_Read(void) {
     ADCON0 &= 0x11000101; //Clearing the Channel Selection Bits
-    ADCON0 |= 0 << 3;     //Setting the required Bits
-    __delay_ms(2);        //Acquisition time to charge hold capacitor
-    GO_nDONE = 1;         //Initializes A/D Conversion
+    ADCON0 |= 0 << 3; //Setting the required Bits
+    __delay_ms(2); //Acquisition time to charge hold capacitor
+    GO_nDONE = 1; //Initializes A/D Conversion
     while (GO_nDONE)
-        ;                            //Wait for A/D Conversion to complete
+        ; //Wait for A/D Conversion to complete
     return ((ADRESH << 8) + ADRESL); //Returns Result
 }
 
-void monitorSolenoidSignal(void)
-{
-    if (isSolenoidOn)
-    {
+void monitorSolenoidSignal(void) {
+    if (isSolenoidOn) {
         return; // Solenoid is currently on so need to monitor its values
     }
 
@@ -55,7 +52,11 @@ void monitorSolenoidSignal(void)
 
     if (count % iterationsPerAverage == 0) //Runs if remainder is 0
     {
-        if (oldMean > 0 && abs(oldMean - mean) >= threshold) // Some condition that says we want to turn on solenoid
+        //       if (oldMean > 0 && abs(oldMean - mean) >= threshold) // Some condition that says we want to turn on solenoid
+        //       {
+        //           turnOnSolenoid();
+        //      }
+        if (mean >= 100) // Some condition that says we want to turn on solenoid
         {
             turnOnSolenoid();
         }
@@ -64,23 +65,20 @@ void monitorSolenoidSignal(void)
     }
 }
 
-void turnOnSolenoid(void)
-{
+void turnOnSolenoid(void) {
     startSolenoidOnClock = clock();
     isSolenoidOn = true;
     RC4 = 1; // Turn on solenoid
     isSolenoidOnMessageTrigger = true;
 }
 
-void isSolenoidOnMonitor(void)
-{
-    if (!isSolenoidOn)
-    {
+void isSolenoidOnMonitor(void) {
+    if (!isSolenoidOn) {
         return; // We didnt trigger solenoid to be on so leave
     }
 
-    clock_t end = clock();
-    double time_taken = ((double)(end - startSolenoidOnClock)) / CLOCKS_PER_SEC; // time ellapsed in seconds
+    time_t end = clock();
+    double time_taken = ((double) (end - startSolenoidOnClock)) / CLOCKS_PER_SEC; // time ellapsed in seconds
 
     if (time_taken > MAX_SOLENOID_ON) // Solenoid has been on for at least MAX_SOLENOID_ON
     {
@@ -90,22 +88,20 @@ void isSolenoidOnMonitor(void)
     }
 }
 
-void sendADCToPhone(void)
-{
-    if (count % iterationsPerAverage == 0)
-    {
-        // UART_send_bite(oldMean); // contiously send the mean to phone for graph
+void sendADCToPhone(void) {
+    if (count % iterationsPerAverage == 0) {
+        char str[30];
+        sprintf(str, "%d", oldMean);
+        UART_send_bite(str); // contiously send the mean to phone for graph
     }
 
-    if (isSolenoidOnMessageTrigger)
-    {
+    if (isSolenoidOnMessageTrigger) {
         UART_send_string("FISH ATTACK");
         UART_send_char(10);
-        UART_send_solenoid_change("1");     // tell phone we are turning on the solenoid
+        UART_send_solenoid_change("1"); // tell phone we are turning on the solenoid
         isSolenoidOnMessageTrigger = false; // We sent the messages we wanted to send so back to false
     }
-    if (isSolenoidOffMessageTrigger)
-    {
+    if (isSolenoidOffMessageTrigger) {
         UART_send_string("FISH ATTACK STOP");
         UART_send_solenoid_change("0"); // tell phone we are turning off the solenoid
 
