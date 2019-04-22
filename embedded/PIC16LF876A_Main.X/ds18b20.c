085b9e6dd07b7c83e3839cefc1cb26ad6fce68b5
NSpoutz <30293581+NSpoutz@users.noreply.github.com>
2019-04-22 10:32:55 -0400

    Update ds18b20.c

--- embedded/PIC16LF876A_Main.X/ds18b20.c
+++ embedded/PIC16LF876A_Main.X/ds18b20.c
@@ -3,116 +3,103 @@
 #include "ds18b20.h"
 #include "counter.h"
 
-
-/*
- * Delay functions for RC3
- */
 int previousTemp = -999;
-//us Delay
 
+//***Delay Functions for GPIO***//
 void delay_us(int useconds) {
     int s;
     for (s = 0; s < useconds; s++);
 }
 
-//ms Delay
-
 void delay_ms(int j) {
     unsigned char i;
     for (; j; j--)
-        for (i = 122; i <= 0; i--);
+    for (i = 122; i <= 0; i--);
 }
 
-/*
- * RESET Pulse for 1-Wire
- */
-
+//***Reseting 1-Wire Data***//
 unsigned char ow_reset(void) {
     DQ_TRIS = 0; // Set RC3 to Output
     DQ = 0; // Set RC3 LOW
     __delay_us(480); // 1-wire required delay
     DQ_TRIS = 1; // Set RC3 HIGH
     __delay_us(60); // 1-wire required delay
-    if (DQ == 0) // If there is a presence pluse
+    if (DQ == 0) // If DS18B20 is connected
     {
         __delay_us(480);
         return 0; // 1-wire is present
-    } else { // If there is NOT a prensence pulse
+    } else { // If DS18B20 is NOT connected
         __delay_us(480);
-        return 1; // 1-wire is NOT present
+        return 1; // Notify DS18B20 is NOT connected
     }
 }
 
-/*
- * Communication Functions for 1-Wire Protocol
- * Refer to Data Sheet for commands  
- */
+//***Communication protocol for DS18B20 (1-Wire)***//
 
-//Read Transmission bits from Temp Sensor
-
+//**Transmission bits from DS18B20**//
 unsigned char read_bit(void) {
     unsigned char i;
-    DQ_TRIS = 1; //Set RC
-    DQ = 0; // pull DQ low to start timeslot
-    DQ_TRIS = 1;
-    DQ = 1; // then return high
-    for (i = 0; i < 3; i++); // delay 15us from start of timeslot
-    return (DQ); // return value of DQ line
+    DQ_TRIS = 1; //Set RC3 to Output
+    DQ = 0; // RC3 LOW
+    DQ_TRIS = 1; //Set RC3 to Output
+    DQ = 1; // Set RC3 HIGH to start data time slot
+    for (i = 0; i < 3; i++); // delay 15us from start for data time slot
+    return (DQ); // Return value of DQ line (HIGH or LOW?)
 }
 
-//Read Transmission bytes from Temp Sensor 
-
+//**Transmission bytes from DS18B20**// 
 unsigned char read_byte(void) {
     char i, result = 0;
-    DQ_TRIS = 1; // TRIS is input(1)
+    DQ_TRIS = 1; // Set RC3 to Input
     for (i = 0; i < 8; i++) {
         DQ_TRIS = 0; // Set RC3 as Output
         DQ = 0; // Set RC3 Low 
         __delay_us(2); //Keep RC3 LOW for 2us
-        DQ_TRIS = 1; // Set RC3 as Input
+        DQ_TRIS = 1; // Set RC3 to Input
         if (DQ != 0) // if Bit is 1 
             result |= 1 << i;
-        __delay_us(60); // wait for recovery time
+        __delay_us(60); 
     }
     return result;
 }
 
+//**Sending bits to DS18B20**//
 void write_bit(char bitval) {
-    DQ_TRIS = 0;
-    DQ = 0; // pull DQ low to start timeslot
+    DQ_TRIS = 0; //Set RC3 to Output
+    DQ = 0; // pull RC3 Low
     if (bitval == 1) DQ = 1; // return DQ high if write 1
-    __delay_us(5); // hold value for remainder of timeslot
+    __delay_us(5); // Ride out rest of time slot 
     DQ_TRIS = 1;
     DQ = 1;
-}// Delay provides 16us per loop, plus 24us. Therefore delay(5) = 104us
+}
 
+//**Sending Bytes to DS18B20**//
 void write_byte(char val) {
     char i;
-    DQ_TRIS = 1; // set pin# to input (1)
+    DQ_TRIS = 1; // Set RC3 to Input
 
     for (i = 0; i < 8; i++) {
         if ((val & (1 << i)) != 0) {
-            // write 1
-            DQ_TRIS = 0; // set pin# to output (0)
-            DQ = 0; // set pin# to low (0)
-            __delay_us(1); // 1 wire require time delay
-            DQ_TRIS = 1; // set pin# to input (release the bus)
-            __delay_us(60); // 1 wire require time delay
+            // Writing HIGH 
+            DQ_TRIS = 0; 
+            DQ = 0; 
+            __delay_us(1); // 1-Wire Delay
+            DQ_TRIS = 1; 
+            __delay_us(60); // 1-Wire Delay
         } else {
-            //write 0
-            DQ_TRIS = 0; // set pin# to output (0)
-            DQ = 0; // set pin# to low (0)
-            __delay_us(60); // 1 wire require time delay
-            DQ_TRIS = 1; // set pin# to input (release the bus)
+            // Writing LOW
+            DQ_TRIS = 0; 
+            DQ = 0; 
+            __delay_us(60); // 1-Wire Delay
+            DQ_TRIS = 1; 
         }
     }
 
 }
 
-/*
- * Initialize ds18b20 and set 12-bit resolution
- */
+//***Setting 12-bit resolution for TEMP Data***//
 
+//**Initialization of DS18B20 (1-Wire)**//
 void ds18b20_Initialize(void) {
     ow_reset();
     write_byte(write_scratchpad);
@@ -121,6 +108,7 @@ void ds18b20_Initialize(void) {
     write_byte(resolution_12bit);
 }
 
+//**Getting and Sending TEMP value**//
 void broadcastTempValue(void) {
     int getTimer = (int) getCounter();
     if (getTimer % 60 != 0) {
@@ -133,6 +121,7 @@ void broadcastTempValue(void) {
     previousTemp = temp;
 }
 
+//**Send TEMP value via UART**//
 void sendTemp(int temp) {
     if (temp != -999 && temp < 100) {
         char str[40];
@@ -141,20 +130,20 @@ void sendTemp(int temp) {
     }
 }
 
-//***Function to read temp data***//
-
+//**Reading TEMP data**//
 unsigned int read_temp(void) {
+    //Checking for DS18B20 connection
     if (ow_reset() == 1) {
         UART_send_string("Temp. NOT connected");
         UART_send_char(10);
         return -999;
     }
+    
+    unsigned short TempL, TempH; //Data will be in 2 bytes
+    int temp = 0; //Setting initial Temp
+    char str[30]; 
 
-    unsigned short TempL, TempH;
-    int temp = 0;
-    char str[30];
-
-    ow_reset();
+    ow_reset(); //Reseting DS18B20
     write_byte(skip_rom);
     write_byte(convert_temp);
 
@@ -170,32 +159,8 @@ unsigned int read_temp(void) {
     TempL = read_byte();
     TempH = read_byte();
 
-    temp = ((unsigned int) TempH << 8) + (unsigned int) TempL; //put both value in one variable
-    temp = temp / 16; //calculations used from the table provided in the data sheet of ds18b20
+    temp = ((unsigned int) TempH << 8) + (unsigned int) TempL; 
+    temp = temp / 16; 
 
     return temp;
-    //i = 0; //I think I need this?
-
-    /*This is for Negative temperature*/
-
-
-    //		if((TempH & 0x80)!=0)
-    //        {    // If condition will execute as TempH = 1111 1111 & 1000 0000 = 1000 0000. 
-    //			t=TempH;// Store tempH value in t = 1111 1111  .
-    //	        t<<=8;//after bitwise left shift 8 times value in t will be 1111 1111 0000 0000.  
-    //	        t=t|TempL;// t = 1111 1111 0000 0000 | 0101 1110 [ result t = 1111 1111 0101 1110] 
-    //	        t=t-1;//t = t-1 in this case t = 1111 1111 0101 1101.
-    //	        t=~t;// t = 0000 0000 1010 0010.
-    //	        t>>=4;// t = 0000 0000 0000 1010.
-    //			t=t*100;// t = 10 * 100 = 1000.
-    //			t2=TempL; //Store tempL value = 0101 1110.
-    //			t2=t2-1;// t2= 0101 1101
-    //	        t2=~t2;//t2 = 1010 0010
-    //	        t2=t2&0x0f;// t2 = 1010 0010 | 0000 1111 = 0000 0010
-    //			t2=t2 * 6.25; // 0000 00010 = 2 x 6.25 = 12.50
-    //			i=((unsigned int)t ) + (unsigned int)t2; //put both value in one variable 1000 + 12.5 = 1012.5
-    //		
-    //
-    //		}
-    /*This is for positive Temperature*/
 }
