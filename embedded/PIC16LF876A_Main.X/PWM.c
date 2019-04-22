8028ff950ccc126e4688d401442eb9594694edb7
NSpoutz <30293581+NSpoutz@users.noreply.github.com>
2019-04-22 11:09:14 -0400

    Update PWM.c

--- embedded/PIC16LF876A_Main.X/PWM.c
+++ embedded/PIC16LF876A_Main.X/PWM.c
@@ -1,10 +1,9 @@
 #include "defines.h"
 #include "PWM.h"
 
-//--[ Duty Cycle % Input Function ]--
+//***Seting Duty Cycle of PWM***///
 void PWM1_Set_Duty(unsigned int DC)
 {
-  // Check The DC Value To Make Sure it's Within 10-Bit Range
   if(DC<1024)
   {
     CCP1Y = DC & 1;
@@ -13,23 +12,26 @@ void PWM1_Set_Duty(unsigned int DC)
   }
 }
 
+//***Initialization of PWM***///
 void PWM_Initialize(void)
 {
-   //--[ Configure The CCP Module For PWM Mode ]--
-  //CPP in PWM Mode
+  //Put Module in PWM Mode
   CCP1M3 = 1;
   CCP1M2 = 1;
-  // The CCP1 Output Pin (PWM) [RC2]
+  
+  //Set RC2 as PWM Pin
   TRISC2 = 0; 
-  // Set The PWM Frequency of 8kHz
+  
+  //Set The PWM Frequency of 8kHz
   PR2 = 124; 
-  // Set The PS For Timer2 (1:4 Ratio)
+  
+  //Timer2 (1:4 Ratio)
   T2CKPS0 = 0;
   T2CKPS1 = 0;
-  // Start CCP1 PWM
-  TMR2ON = 1; //TIMER2 Begins
-  //--[ End of Configuration ]--
   
-  //--[ Set Duty Cycle to 50% ]--
-  PWM1_Set_Duty(400);//Set duty cycle to 50% 
+  // Start PWM Signal with TIMER 2
+  TMR2ON = 1; 
+  
+  //Call PWM Duty Cycle Funciton with value
+  PWM1_Set_Duty(400);//Set duty cycle to 80% 
 }
